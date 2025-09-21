c8c1868d3df11b27eeaca0f9ff90e8a6527b4a57
Martin Valba <kailing2@live.com>
2025-09-21 17:42:49 +0300

    Misc changes

--- shell/docker/docker_common.sh
+++ shell/docker/docker_common.sh
@@ -149,12 +149,6 @@ docker_user_run_cmd() {
 }
 
 docker_container_sysedit() {
-    # echo toor | openssl passwd -1 -stdin
-    #export PASSWD='$1$/M2zf0R0$A21DEu1T5lucDorrkv3vQ0'
-
-    # Give root user passwd so su can be happy
-    docker_run_cmd ${1} 'usermod --password $1$x54QkCxa$cD1GyHo0zjZhmFqUb.2/Y0 root'
-
     # Reset pkg manager sync folder
     docker_run_cmd ${1} "rm -rf /var/lib/${PACKAGE_MANAGER}/sync/*"
 
@@ -162,7 +156,8 @@ docker_container_sysedit() {
     docker_run_cmd ${1} "useradd developer -G adm,wheel -d /home/developer -M -s /usr/bin/bash"
 
     # Give users passwd so su dosent whine about auth info issues
-    docker_run_cmd ${1} 'usermod --password $1$CHsahPBR$S5HFKq./FenLtDyYTcgBK. developer'
+    docker_run_cmd ${1} 'echo "root:toor" | chpasswd'
+    docker_run_cmd ${1} 'echo "developer:developer" | chpasswd'
 
     # Run Essentials before anything else
     docker_run_essentials $1
--- shell/iso_modules/docker_support.sh
+++ shell/iso_modules/docker_support.sh
@@ -7,16 +7,17 @@ docker_iso_setup_env() {
     # Simple check for base image
     docker_image_check
 
+    # Safety check
+    set +e
+    docker_remove_container ${ISO_CONTAINER_NAME} &> /dev/null
+    set -e
+
     # Create iso creator container
     docker_create_container ${ISO_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_ARCH}
 
     # Start container
     docker_start_container ${ISO_CONTAINER_NAME}
     docker_container_sysedit ${ISO_CONTAINER_NAME}
-}
-
-docker_iso_sysedit() {
-    docker_run_cmd ${1} 'usermod --password $1$x54QkCxa$cD1GyHo0zjZhmFqUb.2/Y0 root'
 }
 
 docker_iso_update() {
@@ -26,20 +27,8 @@ docker_iso_update() {
 
 # Basic health check
 docker_iso_checkup() {
-    export CHECKIT1=$(sudo docker container ls -a | grep -wo ${ISO_CONTAINER_NAME})
-
-    if [ -z ${CHECKIT1} ]; then
-        CHECKIT="empty"
-    fi
-
-    if [ ! "${CHECKIT1}" = "${ISO_CONTAINER_NAME}" ]; then
-        docker_iso_setup_env
-        docker_iso_update
-    else
-        docker_start_container ${ISO_CONTAINER_NAME}
-        docker_iso_sysedit
-        docker_iso_update
-    fi
+    docker_iso_setup_env
+    docker_iso_update
 }
 
 docker_iso_build() {
